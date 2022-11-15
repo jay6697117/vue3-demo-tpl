f44b45ae97e766c09c504a33a739dc4d262c881a
steve zhang <871506263@qq.com>
2022-11-15 10:34:42 +0800

    提交代码于: 2022-11-15 10:34:42

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <HelloWorld :msg="undefined" />
+  <HelloWorld :msg="msg" />
 </template>
 
 <script setup lang="ts">
-// import { nanoid } from 'nanoid';
-// import { ref } from 'vue';
+import { nanoid } from 'nanoid';
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+const msg = ref('Hello from parent' + ' - ' + nanoid());
 </script>
 
 <style scoped></style>
