33cda5a050d7389df4c07dffd1e60893f29d0f7c
steve zhang <871506263@qq.com>
2022-11-26 19:09:58 +0800

    提交代码于: 2022-11-26 19:09:58

---
+++ demo/App-8.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>🎉 Congratulations!</h1>
+  <div class="btn-wrap">
+    <button @click="showConfetti">喝彩</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { nanoid } from 'nanoid';
+import JSConfetti from 'js-confetti';
+import { ref } from 'vue';
+const confetti = new JSConfetti();
+
+function showConfetti() {
+  confetti.addConfetti();
+}
+
+showConfetti();
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
+
+.btn-wrap {
+  width: 100%;
+  text-align: center;
+}
+
+button {
+  margin-top: 100px;
+  padding: 2px 20px;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,85 @@
-<template>
-  <h1>🎉 Congratulations!</h1>
-  <div class="btn-wrap">
-    <button @click="showConfetti">喝彩</button>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { nanoid } from 'nanoid';
-import JSConfetti from 'js-confetti';
-import { ref } from 'vue';
-const confetti = new JSConfetti();
-
-function showConfetti() {
-  confetti.addConfetti();
-}
-
-showConfetti();
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
 <style scoped>
-h1 {
-  text-align: center;
-  cursor: pointer;
-  margin-top: 3em;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-.btn-wrap{
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
   width: 100%;
+  font-size: 12px;
   text-align: center;
+  margin-top: 2rem;
 }
 
-button {
-  margin-top: 100px;
-  padding: 2px 20px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
