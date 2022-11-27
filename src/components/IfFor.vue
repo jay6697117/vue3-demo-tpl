d364b53b2b9967d017081be0084f077455f980c2
steve zhang <871506263@qq.com>
2022-11-27 22:46:26 +0800

    提交代码于: 2022-11-27 22:46:26

--- src/components/IfFor.vue
+++ src/components/IfFor.vue
@@ -1,9 +1,68 @@
 <template>
-  <div>111</div>
+  <div class="container">
+    <div class="btns">
+      <button @click="toggleList" :disabled="!(list && list.length)">切换列表</button>
+      <button @click="pushList">加入一个数字</button>
+      <button @click="popList">去掉一个数字</button>
+      <button @click="reverseList">倒序排序</button>
+    </div>
+    <div class="result">list: {{ list }}</div>
+    <div class="list" v-if="isShow && list && list.length">
+      <p v-for="(item, index) in list" :key="index">
+        {{ item }}
+      </p>
+    </div>
+    <div v-else class="list-null">列表为空呢～～～</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {ref,reactive} from 'vue'
+import { ref } from 'vue';
+const isShow = ref(true);
+const list = ref([1, 2, 3]);
+
+function toggleList() {
+  isShow.value = !isShow.value;
+}
+function pushList() {
+  isShow.value = true;
+  const tempArr = list.value;
+  tempArr.push(list.value.length + 1);
+  list.value = tempArr;
+}
+function popList() {
+  isShow.value = true;
+  const tempArr = list.value;
+  tempArr.pop();
+  list.value = tempArr;
+}
+function reverseList() {
+  isShow.value = true;
+  const tempArr = list.value;
+  tempArr.reverse();
+  list.value = tempArr;
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  .btns {
+    button {
+      margin-right: 10px;
+      padding: 2px 10px;
+    }
+  }
+  .result {
+    margin: 20px 0;
+    background-color: #ccc;
+    padding: 6px 10px;
+  }
+  .list {
+    p {
+      margin-bottom: 10px;
+      background-color: rgba(0, 139, 139, 0.5);
+      padding: 6px 10px;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 // import UserInput from '@/components/UserInput.vue';
 // import AttributeBind from '@/components/AttributeBind.vue'
-import IfFor from '@/components/IfFor.vue'
+import IfFor from '@/components/IfFor.vue';
 </script>
 
 <template>
