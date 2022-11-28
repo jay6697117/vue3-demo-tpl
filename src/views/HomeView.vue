81933c170dfbdd5f5abb094b760d626baf7ebd71
steve zhang <871506263@qq.com>
2022-11-28 10:26:59 +0800

    提交代码于: 2022-11-28 10:26:59

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 // import UserInput from '@/components/UserInput.vue';
 // import AttributeBind from '@/components/AttributeBind.vue'
-import IfFor from '@/components/IfFor.vue';
+// import IfFor from '@/components/IfFor.vue';
+import FormBind from '@/components/FormBind.vue';
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
   <!-- <UserInput /> -->
   <!-- <AttributeBind /> -->
-  <IfFor />
+  <!-- <IfFor /> -->
+  <FormBind />
 </template>
