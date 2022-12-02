41bb0129c5628ffe05a190759e91ea3dbb12efc6
steve zhang <871506263@qq.com>
2022-12-02 09:12:36 +0800

    提交代码于: 2022-12-02 09:12:36

--- src/components/FetchData.vue
+++ src/components/FetchData.vue
@@ -2,11 +2,12 @@
   <h1>Latest Vue Core Commits</h1>
   <template v-for="branch in branches">
     <input type="radio" :id="branch" :value="branch" name="branch" v-model="currentBranch" />
-    <label :for="branch">{{ branch }}</label>
+    <label :for="branch">{{ branch }}</label
+    >&nbsp;
   </template>
-  <p>vuejs/vue@{{ currentBranch }}</p>
+  <p>选中分支:&nbsp;&nbsp; {{ currentBranch }}</p>
   <ul>
-    <li v-for="{ html_url, sha, author, commit } in commits">
+    <li v-for="{ html_url, sha, author, commit} in (commits as any)">
       <a :href="html_url" target="_blank" class="commit">{{ sha.slice(0, 7) }}</a>
       - <span class="message">{{ truncate(commit.message) }}</span
       ><br />
@@ -23,24 +24,26 @@
 import { ref, watchEffect } from 'vue';
 
 const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`;
-const branches = ['main', 'v2-compat'];
+const branches = ['main', 'v2-compat', '3.2', 'pnpm'];
 
 const currentBranch = ref(branches[0]);
 const commits = ref(null);
+console.log('commits.value 1:', commits.value);
 
 watchEffect(async () => {
   // 该 effect 会立即运行，
   // 并且在 currentBranch.value 改变时重新运行
   const url = `${API_URL}${currentBranch.value}`;
   commits.value = await (await fetch(url)).json();
+  console.log('commits.value 2:', commits.value);
 });
 
-function truncate(v) {
+function truncate(v: string | string[]) {
   const newline = v.indexOf('\n');
   return newline > 0 ? v.slice(0, newline) : v;
 }
 
-function formatDate(v) {
+function formatDate(v: string) {
   return v.replace(/T|Z/g, ' ');
 }
 </script>
